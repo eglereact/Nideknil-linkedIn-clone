069ff8e9045a997571f836a0d9836fd2c0f8cb96
eglereact <eglereactjs@gmail.com>
2022-04-05 17:01:58 +0300

    adding dark mode

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import logoWhite from "./../images/logoMainDark.png";
+import logoWhite from "./../images/logoMain.png";
+import logoBlue from "./../images/logoMainDark.png";
 import {
   MdHome,
   MdSearch,
@@ -12,17 +13,40 @@ import {
 } from "react-icons/md";
 import { BsMoonFill } from "react-icons/bs";
 import HeaderLink from "./HeaderLink";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { motion } from "framer-motion";
+
+const spring = {
+  type: "spring",
+  stiffness: 700,
+  damping: 30,
+};
 
 function Header() {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme, theme } = useTheme();
+
+  console.log("theme", resolvedTheme);
+  useEffect(() => setMounted(true), []);
+
   return (
     <header
       className="flex items-center sticky top-0 bg-white dark:bg-[#1D2226] z-40 justify-around py-1.5 px-3
     focus-within:shadow-lg"
     >
       <div className="flex items-center space-x-2 w-full max-w-xs">
-        <Image src={logoWhite} width={45} height={45} />
+        {mounted && (
+          <>
+            {resolvedTheme === "dark" ? (
+              <Image src={logoBlue} width={45} height={45} />
+            ) : (
+              <Image src={logoWhite} width={45} height={45} />
+            )}
+          </>
+        )}
         <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
-          <MdSearch />
+          <MdSearch className="text-xl" />
           <input
             type="text"
             placeholder="Search"
@@ -40,17 +64,23 @@ function Header() {
         <HeaderLink Icon={MdGroup} text="Me" feed avartar hidden />
         <HeaderLink Icon={MdOutlineApps} text="Work" feed hidden />
         {/* Dark mode toggle */}
-        <div
-          className={`bg-gray-600 flex- items-center px-1.5 rounded-full h-6 w-12 cursor-pointer 
-          flex-shirk-0 relative`}
-        >
-          <span className="absolute left-1 text-white mt-1">
-            <BsMoonFill />
-          </span>
-          <span className="absolute right-0.5 text-white text-xl mt-0.5">
-            <MdWbSunny />
-          </span>
-        </div>
+        {mounted && (
+          <div
+            onClick={() =>
+              setTheme(resolvedTheme === "dark" ? "light" : "dark")
+            }
+            className={`bg-gray-600 flex items-center px-0.5 rounded-full h-6 w-12 cursor-pointer 
+          flex-shirk-0 relative ${
+            resolvedTheme === "dark" ? "justify-end" : "justify-start"
+          }`}
+          >
+            <motion.div
+              className="w-5 h-5 bg-white rounded-full"
+              layout
+              transition={spring}
+            />
+          </div>
+        )}
       </div>
     </header>
   );
